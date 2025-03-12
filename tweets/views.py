c071bd7b2f500a673af6c3e4a00278edc7358405
유선혁 <tjsgur2399@icloud.com>
2025-03-12 20:19:38 +0900

    challenge4

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
+from http.client import HTTPResponse
+from .models import Tweet
 from django.shortcuts import render
 
 # Create your views here.
+
+def tweet(request):
+    tweets = Tweet.objects.all()
+    return render(request,'tweet.html',{'tweets':tweets,"title":"DjangoChallenge"})
